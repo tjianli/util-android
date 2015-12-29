9fcd40bf199e3c7cbf36325d101964e89d3d7747
tjianli <tjianli@126.com>
2015-12-29 09:58:59 +0800

    no message

--- util/src/main/java/appcoo/util/common/LoadingUtil.java
+++ util/src/main/java/appcoo/util/common/LoadingUtil.java
@@ -2,6 +2,7 @@ package appcoo.util.common;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 /**
  * Created by tjianli on 15/8/11.
@@ -9,10 +10,14 @@ import android.content.Context;
 public class LoadingUtil {
 
     private static ProgressDialog progressDialog;
-    public static void show(Context contex,String msg,boolean cancel){
+    public static void show(Context contex,String msg){
 
         progressDialog = ProgressDialog.show(contex, "", msg, true);
-        progressDialog.setCancelable(cancel);
+        progressDialog.setCancelable(false);
+    }
+
+    public static ProgressDialog getProgressDialog(){
+        return progressDialog;
     }
 
     public static void dismiss(){
@@ -23,5 +28,10 @@ public class LoadingUtil {
     }
     public static void setMessage(String msg){
         progressDialog.setMessage(msg);
+    }
+
+    public static void setCancelListener(DialogInterface.OnCancelListener listener){
+        progressDialog.setCancelable(true);
+        progressDialog.setOnCancelListener(listener);
     }
 }
