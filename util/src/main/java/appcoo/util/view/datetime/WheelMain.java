48b3cda9d5cd9ef9c479d71ed4b4b5c96741f09d
tjianli <tjianli@126.com>
2016-01-07 11:04:20 +0800

    no message

---
+++ util/src/main/java/appcoo/util/view/datetime/WheelMain.java
@@ -0,0 +1,248 @@
+package appcoo.util.view.datetime;
+
+/**
+ * Calendar calendar = Calendar.getInstance();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		
+		String nowString = timeEt.getText().toString();
+		if(nowString.length()<=0){
+			nowString = calendar.get(Calendar.YEAR) + "-" +
+  				    (calendar.get(Calendar.MONTH) + 1 )+ "-" +
+  				    calendar.get(Calendar.DAY_OF_MONTH) + "";
+		}
+		final View timepickerview=LayoutInflater.from(mContext).inflate(R.layout.timepicker, null);
+		ScreenInfo screenInfo = new ScreenInfo(this);
+		final WheelMain wheelMain = new WheelMain(timepickerview,true);
+		wheelMain.screenheight = screenInfo.getHeight();
+		String time = nowString;
+		if(JudgeDate.isDate(time, "yyyy-MM-dd HH:mm")){
+			try {
+				calendar.setTime(dateFormat.parse(time));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int h = calendar.get(Calendar.HOUR_OF_DAY);
+		int m = calendar.get(Calendar.MINUTE);
+		wheelMain.initDateTimePicker(year,month,day,h,m);
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.travel_order_jie_time_hint)
+		.setView(timepickerview)
+		.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				timeEt.setText(wheelMain.getTime());
+			}
+		})
+		.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		})
+		.show();
+ */
+import java.util.Arrays;
+import java.util.List;
+
+import android.view.View;
+
+import appcoo.util.R;
+
+public class WheelMain {
+
+	private View view;
+	private WheelView wv_year;
+	private WheelView wv_month;
+	private WheelView wv_day;
+	private WheelView wv_hours;
+	private WheelView wv_mins;
+	public int screenheight;
+	private boolean hasSelectTime;
+	private static int START_YEAR = 1990, END_YEAR = 2100;
+
+	public View getView() {
+		return view;
+	}
+
+	public void setView(View view) {
+		this.view = view;
+	}
+
+	public static int getSTART_YEAR() {
+		return START_YEAR;
+	}
+
+	public static void setSTART_YEAR(int sTART_YEAR) {
+		START_YEAR = sTART_YEAR;
+	}
+
+	public static int getEND_YEAR() {
+		return END_YEAR;
+	}
+
+	public static void setEND_YEAR(int eND_YEAR) {
+		END_YEAR = eND_YEAR;
+	}
+
+	public WheelMain(View view) {
+		super();
+		this.view = view;
+		hasSelectTime = false;
+		setView(view);
+	}
+	public WheelMain(View view,boolean hasSelectTime) {
+		super();
+		this.view = view;
+		this.hasSelectTime = hasSelectTime;
+		setView(view);
+	}
+	public void initDateTimePicker(int year ,int month,int day){
+		this.initDateTimePicker(year, month, day, 0, 0);
+	}
+	/**
+	 * @Description: TODO 弹出日期时间选择器
+	 */
+	public void initDateTimePicker(int year ,int month ,int day,int h,int m) {
+//		int year = calendar.get(Calendar.YEAR);
+//		int month = calendar.get(Calendar.MONTH);
+//		int day = calendar.get(Calendar.DATE);
+		// 添加大小月月份并将其转换为list,方便之后的判断
+		String[] months_big = { "1", "3", "5", "7", "8", "10", "12" };
+		String[] months_little = { "4", "6", "9", "11" };
+
+		final List<String> list_big = Arrays.asList(months_big);
+		final List<String> list_little = Arrays.asList(months_little);
+
+		// 年
+		wv_year = (WheelView) view.findViewById(R.id.year);
+		wv_year.setAdapter(new NumericWheelAdapter(START_YEAR, END_YEAR));// 设置"年"的显示数据
+		wv_year.setCyclic(true);// 可循环滚动
+		wv_year.setLabel("年");// 添加文字
+		wv_year.setCurrentItem(year - START_YEAR);// 初始化时显示的数据
+
+		// 月
+		wv_month = (WheelView) view.findViewById(R.id.month);
+		wv_month.setAdapter(new NumericWheelAdapter(1, 12));
+		wv_month.setCyclic(true);
+		wv_month.setLabel("月");
+		wv_month.setCurrentItem(month);
+
+		// 日
+		wv_day = (WheelView) view.findViewById(R.id.day);
+		wv_day.setCyclic(true);
+		// 判断大小月及是否闰年,用来确定"日"的数据
+		if (list_big.contains(String.valueOf(month + 1))) {
+			wv_day.setAdapter(new NumericWheelAdapter(1, 31));
+		} else if (list_little.contains(String.valueOf(month + 1))) {
+			wv_day.setAdapter(new NumericWheelAdapter(1, 30));
+		} else {
+			// 闰年
+			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+				wv_day.setAdapter(new NumericWheelAdapter(1, 29));
+			else
+				wv_day.setAdapter(new NumericWheelAdapter(1, 28));
+		}
+		wv_day.setLabel("日");
+		wv_day.setCurrentItem(day - 1);
+
+		wv_hours = (WheelView)view.findViewById(R.id.hour);
+		wv_mins = (WheelView)view.findViewById(R.id.min);
+		if(hasSelectTime){
+			wv_hours.setVisibility(View.VISIBLE);
+			wv_mins.setVisibility(View.VISIBLE);
+			
+			wv_hours.setAdapter(new NumericWheelAdapter(0, 23));
+			wv_hours.setCyclic(true);// 可循环滚动
+			wv_hours.setLabel("时");// 添加文字
+			wv_hours.setCurrentItem(h);
+			
+			
+			wv_mins.setAdapter(new NumericWheelAdapter(0, 59));
+			wv_mins.setCyclic(true);// 可循环滚动
+			wv_mins.setLabel("分");// 添加文字
+			wv_mins.setCurrentItem(m);
+		}else{
+			wv_hours.setVisibility(View.GONE);
+			wv_mins.setVisibility(View.GONE);
+		}
+		
+		// 添加"年"监听
+		OnWheelChangedListener wheelListener_year = new OnWheelChangedListener() {
+			public void onChanged(WheelView wheel, int oldValue, int newValue) {
+				int year_num = newValue + START_YEAR;
+				// 判断大小月及是否闰年,用来确定"日"的数据
+				if (list_big
+						.contains(String.valueOf(wv_month.getCurrentItem() + 1))) {
+					wv_day.setAdapter(new NumericWheelAdapter(1, 31));
+				} else if (list_little.contains(String.valueOf(wv_month
+						.getCurrentItem() + 1))) {
+					wv_day.setAdapter(new NumericWheelAdapter(1, 30));
+				} else {
+					if ((year_num % 4 == 0 && year_num % 100 != 0)
+							|| year_num % 400 == 0)
+						wv_day.setAdapter(new NumericWheelAdapter(1, 29));
+					else
+						wv_day.setAdapter(new NumericWheelAdapter(1, 28));
+				}
+			}
+		};
+		// 添加"月"监听
+		OnWheelChangedListener wheelListener_month = new OnWheelChangedListener() {
+			public void onChanged(WheelView wheel, int oldValue, int newValue) {
+				int month_num = newValue + 1;
+				// 判断大小月及是否闰年,用来确定"日"的数据
+				if (list_big.contains(String.valueOf(month_num))) {
+					wv_day.setAdapter(new NumericWheelAdapter(1, 31));
+				} else if (list_little.contains(String.valueOf(month_num))) {
+					wv_day.setAdapter(new NumericWheelAdapter(1, 30));
+				} else {
+					if (((wv_year.getCurrentItem() + START_YEAR) % 4 == 0 && (wv_year
+							.getCurrentItem() + START_YEAR) % 100 != 0)
+							|| (wv_year.getCurrentItem() + START_YEAR) % 400 == 0)
+						wv_day.setAdapter(new NumericWheelAdapter(1, 29));
+					else
+						wv_day.setAdapter(new NumericWheelAdapter(1, 28));
+				}
+			}
+		};
+		wv_year.addChangingListener(wheelListener_year);
+		wv_month.addChangingListener(wheelListener_month);
+
+		// 根据屏幕密度来指定选择器字体的大小(不同屏幕可能不同)
+		int textSize = 0;
+		if(hasSelectTime)
+			textSize = (screenheight / 100) * 3;
+		else
+			textSize = (screenheight / 100) * 4;
+		wv_day.TEXT_SIZE = textSize;
+		wv_month.TEXT_SIZE = textSize;
+		wv_year.TEXT_SIZE = textSize;
+		wv_hours.TEXT_SIZE = textSize;
+		wv_mins.TEXT_SIZE = textSize;
+
+	}
+
+	public String getTime() {
+		StringBuffer sb = new StringBuffer();
+		int year = wv_year.getCurrentItem() + START_YEAR;
+		int month = wv_month.getCurrentItem() + 1;
+		int day = wv_day.getCurrentItem() + 1;
+		int h = wv_hours.getCurrentItem();
+		int m = wv_mins.getCurrentItem();
+		String yyyy = year+"";
+		String MM = String.format("%02d", month);
+		String dd = String.format("%02d", day);
+		String HH = String.format("%02d", h);
+		String mm = String.format("%02d", m);
+		
+		if(!hasSelectTime)
+			sb.append(yyyy).append("-").append(MM).append("-").append(dd);
+		else
+			sb.append(yyyy).append("-").append(MM).append("-").append(dd).append(" ").append(HH).append(":").append(mm);
+		return sb.toString();
+	}
+}
